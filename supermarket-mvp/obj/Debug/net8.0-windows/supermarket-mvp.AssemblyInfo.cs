296a630fa89eac288e97162ea19a01cf3265714d
Kev-Ram22 <kevinre1122@gmail.com>
2024-10-13 18:17:10 -0500

    Creacion de Presenter para Provider

--- supermarket-mvp/obj/Debug/net8.0-windows/supermarket-mvp.AssemblyInfo.cs
+++ supermarket-mvp/obj/Debug/net8.0-windows/supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9396c8af3153fdf921b9d34d1a2fd0dfbc6acdd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9a2a78c2de48ea33b471acbd360b76fed682593")]
 [assembly: System.Reflection.AssemblyProductAttribute("supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
