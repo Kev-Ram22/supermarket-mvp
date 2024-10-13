afd50db527b3f828efeec2f38a313f5bddb1a92a
Kev-Ram22 <kevinre1122@gmail.com>
2024-10-13 14:46:19 -0500

    Creacion de Presenter para Category

---
+++ supermarket-mvp/Presenter/CategoriePresenter.cs
@@ -0,0 +1,134 @@
+﻿using Supermarket_mvp.Models;
+using Supermarket_mvp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Presenters
+{
+    internal class CategoriePresenter
+    {
+        private ICategoriesView view;
+        private ICategoriesRepository repository;
+        private BindingSource categorieBindingSource;
+        private IEnumerable<CategoriesModel> categorieList;
+
+        public CategoriePresenter(ICategoriesView view, ICategoriesRepository repository)
+        {
+            this.categorieBindingSource = new BindingSource();
+            this.view = view;
+            this.repository = repository;
+
+            this.view.SearchEvent += SearchCategorie;
+            this.view.AddNewEvent += AddNewCategorie;
+            this.view.EditEvent += LoadSelectCategorieToEdit;
+            this.view.DeleteEvent += DeleteSelectedCategorie;
+            this.view.SaveEvent += SaveCategorie;
+            this.view.CancelEvent += CancelAction;
+
+            this.view.SetCategorieListBildingSource(categorieBindingSource);
+            LoadAllCategorieList();
+            this.view.Show();
+        }
+
+        private void LoadAllCategorieList()
+        {
+            categorieList = repository.GetAll();
+            categorieBindingSource.DataSource = categorieList;
+        }
+
+        private void CancelAction(object? sender, EventArgs e)
+        {
+            CleanViewFields();
+        }
+
+        private void SaveCategorie(object? sender, EventArgs e)
+        {
+            var categorie = new CategoriesModel();
+            categorie.Id = Convert.ToInt32(view.CategorieId);
+            categorie.Name = view.CategorieName;
+            categorie.Observation = view.CategorieObservation;
+
+            try
+            {
+                new Common.ModelDataValidation().Validate(categorie);
+                if (view.IsEdit)
+                {
+                    repository.Edit(categorie);
+                    view.Message = "Categorie Edited Successfuly";
+                }
+                else
+                {
+                    repository.Add(categorie);
+                    view.Message = "categorie added Successfuly";
+                }
+                view.IsSuccessful = true;
+
+                CleanViewFields();
+                LoadAllCategorieList();
+
+            }
+            catch (Exception ex)
+            {
+                view.IsSuccessful = false;
+                view.Message = "An Error Ocurred, Could not delete categorie";
+            }
+        }
+
+        private void CleanViewFields()
+        {
+            view.CategorieId = "0";
+            view.CategorieName = "";
+            view.CategorieObservation = "";
+        }
+
+        private void DeleteSelectedCategorie(object? sender, EventArgs e)
+        {
+            try
+            {
+                var categorie = (PayModeModel)categorieBindingSource.Current;
+                repository.Delete(categorie.Id);
+                view.IsSuccessful = true;
+                view.Message = "Categorie deleted succesfuly";
+                LoadAllCategorieList();
+            }
+            catch (Exception ex)
+            {
+                view.IsSuccessful = false;
+                view.Message = "A error ocurred, could not delete categorie";
+            }
+        }
+
+        private void LoadSelectCategorieToEdit(object? sender, EventArgs e)
+        {
+            var categorie = (CategoriesModel)categorieBindingSource.Current;
+
+            view.CategorieId = categorie.Id.ToString();
+            view.CategorieName = categorie.Name;
+            view.CategorieObservation = categorie.Observation;
+
+            view.IsEdit = true;
+        }
+
+        private void AddNewCategorie(object? sender, EventArgs e)
+        {
+            view.IsEdit = false;
+        }
+
+        private void SearchCategorie(object? sender, EventArgs e)
+        {
+            bool emptyValue = string.IsNullOrWhiteSpace(this.view.SearchValue);
+            if (emptyValue == false)
+            {
+                categorieList = repository.GetByValue(this.view.SearchValue);
+            }
+            else
+            {
+                categorieList = repository.GetAll();
+            }
+            categorieBindingSource.DataSource = categorieList;
+        }
+    }
+}
